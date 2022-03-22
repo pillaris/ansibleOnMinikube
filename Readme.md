aa9453de87b63fca95a5828c9f1e8e1855ad5e3d
Matjaž Dolinar <Matjaz.Dolinar@setcce.si>
2022-03-22 09:03:51 +0100

    Fixed issues with user password, Readme update.

--- Readme.md
+++ Readme.md
@@ -16,3 +16,18 @@ Pogoji/prerequisite:
 
 2. zaženi run.sh skripto
 `./run.sh`
+
+## Kako uporabljati okolje
+
+1. Poveži se na node: master
+`kubectl -n ansible-lab exec -it master -- bash`
+
+2. Na worker nodih je narejen uporabnik: ansy:object00
+
+3. Predloga za inventory:
+`
+localhost     ansible_connection=local
+master        ansible_connection=local
+worker01      ansible_user=myuser      ansible_ssh_pass=object00
+worker02      ansible_user=myuser      ansible_ssh_pass=object00
+`
--- run.sh
+++ run.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
-kubectl run master --image=ubuntu --command sleep 1d
-sleep 10
-#kubectl exec -it master -- bash -c "echo nameserver 8.8.8.8 >> /etc/resolv.conf"
-kubectl exec -it master -- bash -c "apt update && apt install -y net-tools ansible sshpass iproute2 vim openssh-server openssh-client"
-kubectl exec -it master -- bash -c "service ssh start"
-kubectl exec -it master -- bash -c "useradd -m -s /bin/bash -p $(echo object00 | openssl passwd -1 -stdin) ansy"
+# If any command fails -> exit
+set -e
 
-kubectl run worker01 --image=ubuntu --command sleep 1d
-sleep 10
-#kubectl exec -it worker01 -- bash -c "echo nameserver 8.8.8.8 >> /etc/resolv.conf"
-kubectl exec -it worker01 -- bash -c "apt update && apt install -y openssh-server openssh-client && service ssh start"
-kubectl exec -it worker01 -- bash -c "useradd -m -s /bin/bash -p $(echo object00 | openssl passwd -1 -stdin) ansy"
-kubectl  expose pod worker01  --port 22
+# Create namespace -> eazyer to clear the env, just run: kubectl delete ns ansible-lab
+kubectl create ns ansible-lab
 
-kubectl run worker02 --image=ubuntu --command sleep 1d
+kubectl -n ansible-lab run master --image=ubuntu --restart=Never --env="DEBIAN_FRONTEND=noninteractive" --env="TZ=Europe/Ljubljana" --command sleep 1d
 sleep 10
-#kubectl exec -it worker02 -- bash -c "echo nameserver 8.8.8.8 >> /etc/resolv.conf"
-kubectl exec -it worker02 -- bash -c "apt update && apt install -y openssh-server openssh-client && service ssh start"
-kubectl exec -it worker02 -- bash -c "useradd -m -s /bin/bash -p $(echo object00 | openssl passwd -1 -stdin) ansy"
-kubectl  expose pod worker02  --port 22
+kubectl -n ansible-lab exec -it master -- bash -c "apt update && apt install -y net-tools ansible sshpass iproute2 vim openssh-server openssh-client"
+kubectl -n ansible-lab exec -it master -- bash -c "service ssh start"
+kubectl -n ansible-lab exec -it master -- bash -c "useradd -m -s /bin/bash -p $(echo object00 | openssl passwd -1 -stdin) ansy"
+
+kubectl -n ansible-lab run worker01 --image=ubuntu --restart=Never --env="DEBIAN_FRONTEND=noninteractive" --env="TZ=Europe/Ljubljana" --command sleep 1d
+sleep 10
+kubectl -n ansible-lab exec -it worker01 -- bash -c "apt update && apt install -y openssh-server openssh-client && service ssh start"
+kubectl -n ansible-lab exec -it worker01 -- bash -c "useradd -m -s /bin/bash ansy && echo 'ansy:object00' | chpasswd"
+kubectl -n ansible-lab expose pod worker01 --port 22
+
+kubectl -n ansible-lab run worker02 --image=ubuntu --restart=Never --env="DEBIAN_FRONTEND=noninteractive" --env="TZ=Europe/Ljubljana" --command sleep 1d
+sleep 10
+kubectl -n ansible-lab exec -it worker02 -- bash -c "apt update && apt install -y openssh-server openssh-client && service ssh start"
+kubectl -n ansible-lab exec -it worker02 -- bash -c "useradd -m -s /bin/bash ansy && echo 'ansy:object00' | chpasswd"
+kubectl -n ansible-lab expose pod worker02 --port 22
+
+set +e
